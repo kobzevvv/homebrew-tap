60ef4c15306a45b31aa90d5ad5d6fdc2bab04242
VLADIMIR KOBZEV <vladimir.kobzev@improvado.io>
2026-02-18 21:54:48 +0000

    chore: bump vibe-sec-app to v1.2.0

--- Casks/vibe-sec-app.rb
+++ Casks/vibe-sec-app.rb
@@ -1,6 +1,6 @@
 cask "vibe-sec-app" do
-  version "1.1.0"
-  sha256 "d016587798818dff35b5023f873b29ce8fdf51325365394a0c9ae981cb45bffd"
+  version "1.2.0"
+  sha256 "f401ff2072a99371f76002526907603710c33ee0cef74937019dd0db51527e9b"
 
   url "https://github.com/kobzevvv/vibe-sec-app/releases/download/v#{version}/VibeSec-#{version}.zip"
   name "VibeSec"
